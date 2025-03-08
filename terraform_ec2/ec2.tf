1c5e084eea69aece8b0fb48340f8fdf4b2ca0c3b
esanolad <esanolad@gmail.com>
2025-03-08 12:25:12 -0500

    adds loadbalancer

--- terraform_ec2/ec2.tf
+++ terraform_ec2/ec2.tf
@@ -1,6 +1,6 @@
 
 
-resource "aws_instance" "my_instance1" {
+resource "aws_instance" "my_instance" {
 
   count = length(data.aws_subnets.default_subnets.ids)
 
@@ -27,8 +27,51 @@ resource "aws_instance" "my_instance1" {
       "sudo apt install apache2 -y",
       "cd /var/www/html/",
       "sudo rm -rf *",
-      "sudo git clone https://github.com/esanolad/rccgmesssiah_website.git ."
+      "echo Welcome to in28minutes - Virtual Server is at ${self.public_dns} | sudo tee /var/www/html/index.html"
     ]
+  }
+}
+
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.elb_security_group.id]
+  subnets            = data.aws_subnets.default_subnets.ids
+
+  enable_deletion_protection = false
+}
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
+}
+
+
+resource "aws_lb_target_group_attachment" "name" {
+  count = length(data.aws_subnets.default_subnets.ids)
+  target_group_arn = aws_lb_target_group.my_target_group.arn
+  target_id = aws_instance.my_instance[count.index].id
+  port = 80
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
   }
 }
 
--- terraform_ec2/network.tf
+++ terraform_ec2/network.tf
@@ -49,3 +49,22 @@ resource "aws_security_group" "http_security_group" {
 
 
 }
+resource "aws_security_group" "elb_security_group" {
+  name = "tf_elb_sg"
+  # vpc_id = aws_vpc.my_aws_vpc.id
+  vpc_id = aws_default_vpc.default.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
